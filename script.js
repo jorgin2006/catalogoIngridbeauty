35d7172ca8676d03b0933a5146d945c2a4521a03
jorgin2006 <jorgefilhosampaio44@gmail.com>
2026-05-08 14:40:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// EFEITO DE ENTRADA
+
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", () => {
+
+  cards.forEach((card) => {
+
+    const top = card.getBoundingClientRect().top;
+
+    if(top < window.innerHeight - 100){
+
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+
+    }
+
+  });
+
+});
+
+// ESTADO INICIAL
+
+cards.forEach((card) => {
+
+  card.style.opacity = "0";
+  card.style.transform = "translateY(40px)";
+  card.style.transition = "0.7s";
+
+});
